a85edce16ba9b205ac893348e7ef24745be76b0d
Ahmed Elaguab <elaguabahmed97@gmail.com>
2021-06-07 02:07:32 +0100

    add main card structure

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,5 +1,16 @@
 import classes from './Card.module.css'
 
 export default function Card() {
-  return <div className={classes.card}>Stats Card Component</div>
+  return (
+    <div className={classes.card}>
+      <header className={classes.card_header}>
+        <img
+          className={classes.card_image}
+          src="/images/image-header-mobile.jpg"
+          href="women working on a table"
+        />
+      </header>
+      <main className={classes.card_body}></main>
+    </div>
+  )
 }
